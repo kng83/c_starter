13552fd0f918cc91bbcd1d706e758dba1d891074
kng83 <pawel.kng@gmail.com>
2019-05-14 17:08:18 +0200

    some

--- hello.c
+++ hello.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
+#define  IMIE "Bobo"
 int main(){
-    char kot[10] = "Tsaaw\a\n";
-    printf("to jest %s\n",kot);
-    printf("to jest %d\n",sizeof(kot));
-    printf("to jest wysow \f i co sie stalo");
-    printf("to jest %d",sizeof(kot));
-    unsigned short int kon =4;
-    printf("\nto jes dlugosc kon %i",sizeof(kon));
-    char alpha = 'A';
-    printf("\n size of alpha %i",sizeof(alpha));
+
+//test sizeof
+char name[20]= "Pawles";
+printf("\nsizeof my name %i",sizeof(name));
+
+//test strlen
+printf("\nsizeof my name %i",strlen(name));
+
+//sprawdzam define
+printf("\nPiotrek nic nie robi %s",IMIE);
 
 }
  
---
+++ sprawdzenie_typow.c
@@ -0,0 +1,20 @@
+   unsigned short int kon =4;
+    printf("\nto jes dlugosc kon %i",sizeof(kon));
+    char alpha = 'A';
+    printf("\n size of alpha %i",sizeof(alpha));
+    float piotrek = 10.0;
+    printf("\n typ float wartosc to %d",sizeof(piotrek));
+    long long dar = 10.0;
+    printf("\n typ long long wartosc to %d",sizeof(dar));
+    long  pojlong = 10.0;
+    printf("\n typ poj long wartosc to %d",sizeof(pojlong));
+    double  doub = 10.0;
+    printf("\n typ double wartosc to %d",sizeof(doub));
+    printf("\n callin some funciton %c",goSomeWhere());
+    printf("\n %d %d %d",4,3);
+
+//tu mamy floata zaokroglonego do 3 miejsc po przecinku;
+    float ryba =10.134132;
+    printf("\n to jes %.3f",ryba);
+}
+ 
