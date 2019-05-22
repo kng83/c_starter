69b1dc3818b3d2874c4f7deb3d3c38078c16cfed
kng83 <pawel.kng@gmail.com>
2019-05-22 22:38:06 +0200

    end of the day

--- million/game.h
+++ million/game.h
@@ -2,18 +2,20 @@
 #include <ctype.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 //**Constanst
 #define MAX_LEVEL 10
 #define MAX_QUESTION_ARR 20
+#define MAX_QUESTION_LIST 300
+#define SEMI_NUMBER 6
 
 //**Declarations
 void initializeRandom();
 void playGame();
 void findQuestion(int level);
 DataModel  readLineByQuestionLevel(int level);
-void findQuestionByLevel(FILE *fp, DataModel *questionsByLevel, int level);
-void mapperArray(FILE * fp, long arr[][6]);
+void mapperArray(FILE * fp, long arr[][8]);
 
 //**Main
 void beginGame(){
@@ -25,8 +27,28 @@ void beginGame(){
 void playGame(){
     int level;
     printf("\n[1]******* Rozpoczynamy gre ***************************************");
+
+    FILE *fp; //file pointer
+    //**Check data file;
+    if ((fp = fopen("data.csv", "r")) == NULL){
+        fprintf(stdin, "Nie moge otworzyc pliku \" words \".\n");
+        badRoute();
+    }
+    long mappedArr[MAX_QUESTION_LIST][8];
+
+    printf("\n");
+    mapperArray(fp,mappedArr);
+
+    for(int i=0L;i<8;i++){
+        for(int j = 0L;j<8;j++){
+             printf(" %lu",mappedArr[i][j]);
+        }
+        printf("\n");
+    }
+   
+
     for(level =1 ; level <MAX_LEVEL; level++ ){
-        findQuestion(level);
+        ;
     }
  
 }
@@ -59,53 +81,24 @@ DataModel  readLineByQuestionLevel(int level){
         fprintf(stdin, "Nie moge otworzyc pliku \" words \".\n");
         badRoute();
     }
-    findQuestionByLevel(fp,questions,level);
-
     fclose(fp);
 
     return question;
 }
 
-void findQuestionByLevel(FILE *fp, DataModel * questionsByLevel,int level){
-    char ch;
-    char content[ANSWER];
-    int shortCounter,colonCounter;
-    int questionsIndex =0;
-    long counter, end_position, new_line_position, first_semicolon_in_line;
-    
-    fseek(fp,0L,SEEK_END);//idz na koniec pliku
-    end_position = ftell(fp); //pozycja na koncu
-    fseek(fp, 0L, SEEK_SET);  //na poczatek pliku
 
-    counter =0 ;
-    shortCounter = 0;
-     for (counter; counter <= end_position; counter++){
-        //**Find last semicolon and of line
-        fseek(fp, counter,0L);
-        ch = getc(fp);
-        if(ch!=';' || ch!='\n'){
-            content[shortCounter] = ch;
-            shortCounter ++;
-        }
-        if(ch==';'){
-            shortCounter=0; //aby poczatek by w zerze
-        }
-    
-        printf("%c",ch);
-        }
-    }
 
 void initializeRandom(){
      srand(time(NULL));   // Initialization, should only be called once.
 }
 
-void mapperArray(FILE * fp, long arr[][6]){
+void mapperArray(FILE * fp, long arr[][8]){
     //***Array element look like this 
     //[level][correct_answer_position][fake_answer_pos][fake_2][fake_3][end_position]
     //1;1;g;b;b;b;
     //***
     char ch;
-    char content[ANSWER];
+    char content[100];
     int shortCounter,colonCounter,newLineCounter;
     int questionsIndex =0;
     long counter, end_position, new_line_position, first_semicolon_in_line;
@@ -131,23 +124,31 @@ void mapperArray(FILE * fp, long arr[][6]){
             colonCounter++;
             shortCounter=0; //aby poczatek by w zerze
         }
-        if(colonCounter==1){
-            arr[new_line_position][0L]= atol(content);
+        //index
+        if(colonCounter==1 && ch==';'){
+            arr[newLineCounter][0]= atol(content);
+         //   strcpy(content,"0000");
+
+        }
+        //poziom
+        if(colonCounter==2 && ch==';'){
+            arr[newLineCounter][1]= atol(content);
+          //   strcpy(content,"0000");
 
         }
         //aby wylapac pozycje wszystkie
-        if(colonCounter >1){
-            arr[new_line_position][colonCounter] = counter;
+        if(colonCounter >2 && ch==';'){
+            arr[newLineCounter][colonCounter] = counter;
         }
 
         if(ch=='\n'){
+            printf("new line is here %lu\n",counter);
+            counter++; //aby ominac podwojny koniec lini
             shortCounter=0;
             colonCounter=0;
-            new_line_position++;
-        }
-  
-        printf("%c",ch);
-        
+            newLineCounter++;
+        }        
+        //printf("end %lu newline %lu",counter,newLineCounter);
     }
 
 }
