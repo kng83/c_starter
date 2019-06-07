4cfa2875bbfe37e44579c003f1ac1334edb10828
kng83 <pawel.kng@gmail.com>
2019-06-07 20:37:44 +0200

    s

--- db/test.c
+++ db/test.c
@@ -5,35 +5,34 @@
 #include "types.h"
 #include "db_func.h"
 
-
-
-//declarations
-extern void* findKeyInArr(size_t* arr_ptr, int elm_size, char* key); 
+// declarations
+extern void* findKeyInArr(size_t* arr_ptr, int elm_size, char* key);
 
 int main() {
     ptr_Arr mArr[CELLS];
     size_t* ptr;
     ptr = adv_malloc(sizeof mArr);
 
-    //Fake pointer area
+    // Fake pointer area
     int one = 1;
     int two = 2;
     int three = 3;
-    int four =4;
+    int four = 4;
     size_t* ptr_one;
     size_t* ptr_two;
     size_t* ptr_three;
     size_t* ptr_four;
     ptr_one = (size_t*)&one;
     ptr_two = (size_t*)&two;
-    ptr_three =(size_t*)&three;
+    ptr_three = (size_t*)&three;
     ptr_four = (size_t*)&four;
 
-    char *keyTrueArr[10] = {"Bobo","Com","Czas","Dom","Gom","Kret","Net","Orka","Pies","Ziom"};
-    char *keyFakeArr[21] = {
-        "Ara","Bobos","Bzw","Cam","Czaz","Dama",
-        "Drozd","Game","Got","Hret","Krat","Krew","Nat","Netia",
-        "Olcha","Ospa","Pack","Piesek","Pwc","Zam","Zka"};
+    char* keyTrueArr[10] = {"Bobo", "Com", "Czas", "Dom",  "Gom",
+                            "Kret", "Net", "Orka", "Pies", "Ziom"};
+    char* keyFakeArr[21] = {"Ara",   "Bobos", "Bzw",   "Cam",  "Czaz", "Dama",
+                            "Drozd", "Game",  "Got",   "Hret", "Krat", "Krew",
+                            "Nat",   "Netia", "Olcha", "Ospa", "Pack", "Piesek",
+                            "Pwc",   "Zam",   "Zka"};
 
     printf("\nsize is %i", adv_allocated_size(ptr));
     strcpy(((ptr_Arr*)(ptr + 0))->key, "Bobo");
@@ -58,41 +57,41 @@ int main() {
     // strcpy((ptr + 8)->key, "Pies");
     // strcpy((ptr + 9)->key, "Ziom");
 
-   // pointer assignment
+    // pointer assignment
     //  ((ptr_Arr*)(ptr + 0)).value_ptr = ptr_one;
     //  ((ptr_Arr*)(ptr + 1)).value_ptr = ptr_two;
     //  ((ptr_Arr*)(ptr + 2)).value_ptr = ptr_three;
     //  ((ptr_Arr*)ptr + 3)).value_ptr = ptr_four;
 
-
-    //test true array 
-    size_t *ret_ptr= NULL;
-    printf("\nPointer starts at %p",ret_ptr);
+    // test true array
+    size_t* ret_ptr = NULL;
+    printf("\nPointer starts at %p", ret_ptr);
     printf("\n----------------begin good------------------\n");
-    for(int i=0;i<10;i++){
-        printf("\nfinding key %s",keyTrueArr[i]);     
+    for (int i = 0; i < 10; i++) {
+        printf("\nfinding key %s", keyTrueArr[i]);
         ret_ptr = findKeyInArr(ptr, sizeof(ptr_Arr), keyTrueArr[i]);
-        printf("\nPointer %p",ret_ptr);
+        printf("\nPointer %p", ret_ptr);
         printf("\n---------------------\n");
     }
-     printf("\n----------------begin fake------------------\n");
-    for(int i=0;i<21;i++){
-        printf("\nfinding key %s",keyFakeArr[i]);
+    printf("\n----------------begin fake------------------\n");
+    for (int i = 0; i < 21; i++) {
+        printf("\nfinding key %s", keyFakeArr[i]);
         ret_ptr = findKeyInArr(ptr, sizeof(ptr_Arr), keyFakeArr[i]);
-        printf("\nPointer %p",ret_ptr);
+        printf("\nPointer %p", ret_ptr);
         printf("\n---------------------\n");
     }
 
-    //Pointer print
+    // Pointer print
     // printf("\nPtr_one %p",((ptr_Arr*)(ptr + 0))->value_ptr);
     // printf("\nPtr_two %p",((ptr_Arr*)(ptr + 1))->value_ptr);
     // printf("\nPtr_three %p",((ptr_Arr*)(ptr + 2))->value_ptr);
     // printf("\nPtr_four %p",((ptr_Arr*)(ptr + 3))->value_ptr);
 
-    //print struct element
-     printf("\nKey_one %s",((ptr_Arr*)(ptr + 0))->key);
-     printf("\nKey_one %s",mArr[0].key);
-     printf("\nKey_one %s",mArr[1].key);
-     printf("\nKey_one %s",mArr[2].key);
+    // print struct element
+    printf("\nKey_0 %s", ((ptr_Arr*)(ptr + 0))->key);
+    printf("\nKey_0 address %p", &(((ptr_Arr*)(ptr + 0))->key));
+    printf("\nKey_1 %s", ((ptr_Arr*)(ptr + 1))->key);
+    printf("\nKey_1 address %p", &(((ptr_Arr*)(ptr + 1))->key));
+    printf("\nKey_2 %s", ((ptr_Arr*)(ptr + 2))->key);
+    printf("\nKey_2 address %p", &(((ptr_Arr*)(ptr + 2))->key));
 }
-
